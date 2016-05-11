4f09417dd44e26d914616dbb09b2cce4031de6c0
lep828 <lep828@gmail.com>
2016-05-11 09:35:48 +0100

    playing with the sidebar

--- public/js/client.js
+++ public/js/client.js
@@ -273,8 +273,38 @@ Zombie.autocomplete = function() {
   });
 }
 
-Zombie.initialize = function(){
+Zombie.setupSidebar = function() {
+  var trigger = $('.hamburger'),
+  overlay = $('.overlay'),
+  isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();      
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {          
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {   
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+    $('#wrapper').toggleClass('toggled');
+  });
+}
+
+Zombie.initialize = function() {
   // $('#getUsers').on('click', this.getUsers);
+  this.setupSidebar();
   this.setupGoogleMaps();
   this.setupNavigation();
   this.setupForm();
