1697e0a9aef01efa6f0564cf0842d102b5cf4577
BellaT <bella_taylor@hotmail.co.uk>
2016-05-12 11:20:50 +0100

    added some more markers around the world

--- db/seeds.js
+++ db/seeds.js
@@ -6,6 +6,8 @@ mongoose.connect(databaseURL);
 var Marker = require("../models/marker"); 
 var User    = require("../models/user");
 
+Marker.collection.drop();
+
 var newYork = new Marker({
   image: "./photos/zombie1.jpg",
   lat: "40.7128",
@@ -39,13 +41,13 @@ buenosAires.save(function(err, marker) {
  console.log("Marker saved! ", marker);
 })
 
-var romania = new Marker({
+var austria = new Marker({
   image: "./photos/zombie22.jpg",
-  lat: "44.403296",
-  lng: "26.127261"
+  lat: "48.288208",
+  lng: "16.256555"
 })
 
-romania.save(function(err, marker) {
+austria.save(function(err, marker) {
  if (err) return console.log(err);
  console.log("Marker saved! ", marker);
 })
@@ -60,3 +62,116 @@ japan.save(function(err, marker) {
  if (err) return console.log(err);
  console.log("Marker saved! ", marker);
 })
+
+var california = new Marker({
+  image: "./photos/zombie24.jpg",
+  lat: "36.632449",
+  lng: "-121.701371"
+})
+
+california.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var glastonbury = new Marker({
+  image: "./photos/zombie7.jpg",
+  lat: "51.156002",
+  lng: "-2.586359"
+})
+
+glastonbury.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var london = new Marker({
+  image: "./photos/zombie_11.jpg",
+  lat: "51.514942",
+  lng: "-0.089627"
+})
+
+london.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var capetown = new Marker({
+  image: "./photos/zombie5.jpg",
+  lat: "-33.869025",
+  lng: "18.676261"
+})
+
+capetown.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var kazakhstan = new Marker({
+  image: "./photos/zombie4.jpg",
+  lat: "51.288677",
+  lng: "71.537695"
+})
+
+kazakhstan.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var canada = new Marker({
+  image: "./photos/zombie21.jpg",
+  lat: "53.580015",
+  lng: "-113.531063"
+})
+
+canada.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var germany = new Marker({
+  image: "./photos/zombie25.jpg",
+  lat: "52.534084",
+  lng: "13.421209"
+})
+
+germany.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var sweden = new Marker({
+  image: "./photos/zombie8.png",
+  lat: "66.593239",
+  lng: "19.890177"
+})
+
+sweden.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var chicago = new Marker({
+  image: "./photos/zombie6.jpg",
+  lat: "41.837484",
+  lng: "-87.691002"
+})
+
+chicago.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+var india = new Marker({
+  image: "./photos/zombie26.jpg",
+  lat: "19.158408",
+  lng: "73.219123"
+})
+
+india.save(function(err, marker) {
+ if (err) return console.log(err);
+ console.log("Marker saved! ", marker);
+})
+
+
+ 
--- public/js/client.js
+++ public/js/client.js
@@ -68,7 +68,7 @@ Zombie.setupGoogleMaps = function(){
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"simplified"},{"saturation":"-100"},{"invert_lightness":true},{"lightness":"11"},{"gamma":"1.27"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"hue":"#ff0000"},{"visibility":"simplified"},{"invert_lightness":true},{"lightness":"-10"},{"gamma":"0.54"},{"saturation":"45"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"},{"saturation":"75"},{"lightness":"24"},{"gamma":"0.70"},{"invert_lightness":true}]},{"featureType":"poi.government","elementType":"all","stylers":[{"hue":"#ff0000"},{"visibility":"simplified"},{"invert_lightness":true},{"lightness":"-24"},{"gamma":"0.59"},{"saturation":"59"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"simplified"},{"invert_lightness":true},{"hue":"#ff0000"},{"saturation":"73"},{"lightness":"-24"},{"gamma":"0.59"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"lightness":"-41"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"},{"invert_lightness":true},{"saturation":"43"},{"lightness":"-16"},{"gamma":"0.73"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"hue":"#ff0000"},{"saturation":"43"},{"lightness":"-11"},{"gamma":"0.73"},{"invert_lightness":true}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"45"},{"lightness":"53"},{"gamma":"0.67"},{"invert_lightness":true},{"hue":"#ff0000"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#ff0000"},{"saturation":"38"},{"lightness":"-16"},{"gamma":"0.86"}]}],
     mapTypeControl: false,
-    minZoom: 3
+    minZoom: 2
   }
 
   this.map = new google.maps.Map(this.canvas, mapOptions);
@@ -343,7 +343,6 @@ Zombie.createFakeMarkers = function(image, lat, lng){
     icon: "./images/zombie-outbreak.png",
     position: city,
     map: Zombie.map,
-    title: 'New York',
   });
 
   marker.addListener('click', function() {
@@ -429,7 +428,7 @@ Zombie.setupSidebar = function() {
 }
 
 Zombie.setupModal = function() {
-  $("#story-modal").modal('show');
+  $("#story-modal").modal('show').fadeIn(800);
 }
 
 Zombie.appendVideos = function(data) {
