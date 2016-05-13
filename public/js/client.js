e64bbdd9db40e0b6b9299c19151f3b908995cfc6
lep828 <lep828@gmail.com>
2016-05-13 10:45:16 +0100

    made counter size bigger and decreased the looping time interval

--- public/js/client.js
+++ public/js/client.js
@@ -58,13 +58,13 @@ Zombie.getUsers = function() {
 }
 
 Zombie.iconTypes = [
-"airport",
-"campground",
-"hospital",
-"hardware_store",
-"pharmacy",
-"doctor",
-"police"
+  "airport",
+  "campground",
+  "hospital",
+  "hardware_store",
+  "pharmacy",
+  "doctor",
+  "police"
 ]
 
 Zombie.setupGoogleMaps = function(){
@@ -100,7 +100,6 @@ Zombie.getTemplate = function(tpl, data) {
       Zombie.autocomplete();
       Zombie.requestFakeMarkers();
       Zombie.setupHeatmap();
-      Zombie.getDate();
       if (!Zombie.loaded) {
         Zombie.setupModal();
         Zombie.loaded = true;
@@ -527,7 +526,7 @@ Zombie.getDate = function() {
 
 Zombie.count = function() {
   document.getElementById('counter').innerHTML = this.number;
-  this.number += parseInt(this.number/1337);
+  this.number += parseInt(this.number / 1337);
 }
 
 Zombie.initialize = function() {
@@ -537,7 +536,7 @@ Zombie.initialize = function() {
   this.setupForm();
   this.setupAudio();
 
-  setInterval('Zombie.count()', 80);
+  setInterval('Zombie.count()', 200);
 }
 
 $(function(){
